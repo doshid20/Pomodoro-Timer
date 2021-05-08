df5624d5c95efa22b0b29b2f6512fa769975c508
doshid20 <79118790+doshid20@users.noreply.github.com>
2021-05-08 11:14:33 -0700

    Add files via upload

---
+++ build/precache-manifest.14e8ebdf9a3e10bf67f64becdebfc69b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9645541554bc44f2e84df822f97212b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac245fcd51a1c11dd91a",
+    "url": "/static/css/main.6aba0a52.chunk.css"
+  },
+  {
+    "revision": "1965870259743c439629",
+    "url": "/static/js/2.01fc0311.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.01fc0311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac245fcd51a1c11dd91a",
+    "url": "/static/js/main.f9163f8f.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
